2205cf0520b5fa1d7ff8d66f64e03f5542790d23
mir <putra.abi@gmail.com>
2021-07-12 17:36:11 +0700

    Header update
    Icon display
    Still not bouncing, havent figured out why

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,33 @@
-function header() {
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon, 
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from '@heroicons/react/outline'
+import Image from 'next/image'
+import HeaderItem from './HeaderItem'
+
+function Header() {
     return (
-        <header className=''>
-            <h1>This is header</h1>
+        <header className="">
+            <div>
+                <HeaderItem title="HOME" Icon={HomeIcon}/>
+                <HeaderItem title="TRENDING'" Icon={LightningBoltIcon}/>
+                <HeaderItem title="VERFIED" Icon={BadgeCheckIcon}/>
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon}/>
+                <HeaderItem title="SEARCH" Icon={SearchIcon}/>
+                <HeaderItem title="ACCOUNT" Icon={UserIcon}/>
+            </div>
+            <Image 
+                className="object-contain"    
+                src="https://i.ibb.co/GJMwK4F/TMOVI.png" 
+                width={200}
+                height={100}
+            />
         </header>
     )
 }
 
-export default header
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
+import Header from "../components/Header"
 
 export default function Home() {
   return (
@@ -11,7 +11,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <h1>Tmovi</h1>
+    <h1></h1>
+
+    {/*  Header  */}
+    <Header />
+    {/*  Navbar  */}
+    {/*  Result  */}
     </div>
   )
 }
